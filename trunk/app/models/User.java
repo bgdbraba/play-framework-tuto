dcd16983b599a6a566d14909385108099de4b851
poutsjr@gmail.com <poutsjr@gmail.com@cd9721f3-921c-c9f7-8795-7b439047d428>
2011-08-28 21:27:35 +0000

    first page

--- trunk/app/models/User.java
+++ trunk/app/models/User.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.util.*;
+
 import javax.persistence.*;
 
 import play.db.jpa.*;
@@ -10,14 +11,21 @@ public class User extends Model {
 
 	public String email;
 	public String password;
-	public String fullname;
-	public boolean isAdmin;
+	public String firstname;
+	public String lastname;
+	public Date birthdate;
 
-	public User(String email, String password, String fullname) {
+
+	public User(String email, String password, String firstname,
+			String lastname, Date birthdate) {
+		super();
 		this.email = email;
 		this.password = password;
-		this.fullname = fullname;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.birthdate = birthdate;
 	}
+
 
 	public static User connect(String email, String password) {
 		return find("byEmailAndPassword", email, password).first();
