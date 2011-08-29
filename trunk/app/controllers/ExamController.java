65ce40de77fa9b1bc15f145ae476bd2de6d20b37
poutsjr@gmail.com <poutsjr@gmail.com@cd9721f3-921c-c9f7-8795-7b439047d428>
2011-08-29 21:14:07 +0000

    exam_go_2

--- trunk/app/controllers/ExamController.java
+++ trunk/app/controllers/ExamController.java
@@ -1,12 +1,13 @@
 package controllers;
 
-import play.*;
+import models.Exam;
+import models.Post;
+import models.Question;
+import models.Question.QuestionType;
+import play.Play;
 import play.data.validation.Required;
-import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
+import play.mvc.Before;
+import play.mvc.Controller;
 
 public class ExamController extends Controller {
 
@@ -15,18 +16,36 @@ public class ExamController extends Controller {
 		render(exam);
 	}
 
+	public static void beginQuestionnaire(String examKey) {
+		Exam exam = Exam.findByKey(examKey);
+		exam.nextQuestion();
+		Question question = exam.questionnaire.questions.get(exam.currentQuestion);
+		System.out.println(question.questionType);
+		render(exam, question);
+	}
+	
+	public static void nextQuestion(String examKey) {
+		beginQuestionnaire(examKey);
+	}
+
 	public static void show(Long id) {
 		Post post = Post.findById(id);
 		render(post);
 	}
 
-	public static void storeCandidate(Long examId, @Required String email,
-			@Required String password, @Required String confirmPassword, Date birthdate) {
+	public static void storeCandidate(Long examId, @Required String password, @Required String confirm_password,
+			String birthdate) {
 		Exam exam = Exam.findById(examId);
+		System.out.println(examId);
+		exam.candidate.password = password;
+		exam.candidate.save();
+//		System.out.println("password " + password);
+//		System.out.println("confirm " + confirm_password);
+//		System.out.println("birth " + birthdate);
 		if (validation.hasErrors()) {
 			render("ExamController/go.html", exam);
 		}
-		go(exam.examKey);
+		beginQuestionnaire(exam.examKey);
 	}
 
 	@Before
--- trunk/app/models/Exam.java
+++ trunk/app/models/Exam.java
@@ -3,19 +3,38 @@ package models;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 import play.db.jpa.Model;
 
 @Entity
 public class Exam extends Model {
 
+	@ManyToOne
 	public User candidate;
+
+	@ManyToOne
 	public Questionnaire questionnaire;
+
 	public Date creationDate;
+	public Date startingDate;
 	public String examKey;
 	public int currentQuestion;
-	
-	public static Exam findByKey(String examKey){
+
+	public Exam() {
+		creationDate = new Date();
+		currentQuestion = -1;
+	}
+
+	public static Exam findByKey(String examKey) {
 		return find("byExamKey", examKey).first();
+	}
+
+	public void nextQuestion() {
+		if (startingDate == null) {
+			startingDate = new Date();
+		}
+		//currentQuestion++;
+		this.save();
 	}
 }
